b89adf563c93f99a3014800124411069344a183d
matteo <matteo.bastide06@gmail.com>
2025-11-17 20:44:56 +0100

    [+]optimisation du code (Back)

--- src/main.go
+++ src/main.go
@@ -16,6 +16,11 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	r.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	fmt.Println("Serveur demarré sur http://localhost:8080")
+	// Couleurs ANSI
+	blue := "\033[34m"
+	reset := "\033[0m"
+
+	fmt.Println("Serveur démarré sur", blue+"http://localhost:8080"+reset)
+
 	http.ListenAndServe(":8080", r)
 }
